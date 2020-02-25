2704f6994c26d5c2c96b86b3b46cb23a030948f8
matveyka-voron <matveyka.voron@yandex.ru>
2020-02-25 16:26:54 +0700

    files for project

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def openVk():
     webbrowser.open('https://vk.com/drift_gta')
 
 
+def downloadGtaSa():
+    webbrowser.open('https://gta-max.com/758-skachat_gta_san_andreas_original.html')
+
+
+def downloadSAMP():
+    webbrowser.open('https://gta-max.com/576-skachat_sa_mp_0_3_7_samp.html')
+
+
 window = Tk()
 window.title('"GTA SAMP • РУССКИЙ ДРИФТ СЕРВЕР • RDS •" - Launcher 32bit')
 window.resizable(width=False, height=False)
@@ -21,7 +29,8 @@ one = Label(window, image=photo)
 one.image = photo  # just keeping a reference
 one.grid()
 
-butNews = Button(window, text='Смотреть новости проекта')
+butNews = Button(window, text='''Смотреть новости
+проекта SAMP''')
 butNews.config(width=22, height=2, bg='gainsboro', fg='black', command=openNews)
 butNews.place(x=470, y=80)
 
@@ -29,5 +38,15 @@ butVk = Button(window, text='''Страница сервера
 GTA SAMP 0.3.7''')
 butVk.config(width=22, height=2, bg='gainsboro', fg='black', command=openVk)
 butVk.place(x=470, y=122)
+
+butDownloadGtaSa = Button(window, text='''Загрузить чистую
+версию GTA SA''')
+butDownloadGtaSa.config(width=22, height=2, bg='gainsboro', fg='black', command=downloadGtaSa)
+butDownloadGtaSa.place(x=470, y=164)
+
+butDownloadSAMP = Button(window, text='''Загрузить клиент
+SAMP 0.3.7''')
+butDownloadSAMP.config(width=22, height=2, bg='gainsboro', fg='black', command=downloadSAMP)
+butDownloadSAMP.place(x=470, y=206)
 
 window.mainloop()
