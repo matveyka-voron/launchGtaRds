206461d4ef6d1fab7532a461d103c0a5cfa71549
matveyka-voron <matveyka.voron@yandex.ru>
2020-02-25 16:15:55 +0700

    files for project

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from tkinter import *
 import webbrowser, os
 
 
+def openNews():
+    webbrowser.open('https://vk.com/drift_gta/update?act=s&id=18366511')
+
+
+def openVk():
+    webbrowser.open('https://vk.com/drift_gta')
+
+
 window = Tk()
 window.title('"GTA SAMP • РУССКИЙ ДРИФТ СЕРВЕР • RDS •" - Launcher 32bit')
 window.resizable(width=False, height=False)
@@ -13,6 +21,13 @@ one = Label(window, image=photo)
 one.image = photo  # just keeping a reference
 one.grid()
 
+butNews = Button(window, text='Смотреть новости проекта')
+butNews.config(width=22, height=2, bg='gainsboro', fg='black', command=openNews)
+butNews.place(x=470, y=80)
 
+butVk = Button(window, text='''Страница сервера
+GTA SAMP 0.3.7''')
+butVk.config(width=22, height=2, bg='gainsboro', fg='black', command=openVk)
+butVk.place(x=470, y=122)
 
 window.mainloop()
